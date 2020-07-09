d694d3ed4f4cd05dea78acbdb26d44c7dab301c9
Neven Bedr <neven2225@gmail.com>
2020-07-09 18:02:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const match = drivers.filter(d => d.toUpperCase() === name.toUpperCase())
+
+    if(match !== undefined){
+      return match
+    }
+
+    return [];
+  };
+
+function fuzzyMatch(drivers, letters){
+  const match = drivers.filter(drivers => drivers.substring(0,2) === letters)
+
+  if(match !== undefined){
+    return match;
+  }
+
+  return [];
+}
+
+function matchName(drivers, name){
+const match = drivers.filter(drivers => drivers.name === name)
+
+if(match !== undefined){
+  return match
+}
+
+return [];
+
+}
